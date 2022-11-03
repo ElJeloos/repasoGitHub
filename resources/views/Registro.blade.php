6dfd0dc4a27e91de3932584fb7a40e7a6ccac025
ElJeloos <120036576@upq.edu.mx>
2022-11-03 01:00:22 -0600

    Primera parte
    vistas creadas y controladores, vista principal casi finalizada, aspectos basicos listos

---
+++ resources/views/Registro.blade.php
@@ -0,0 +1,20 @@
+@extends('plantilla')
+
+
+@section('contenido')
+
+
+ <div class="container mt-4 col-md-7">
+  
+  <div class="card text-center mb-5" style="width: 34rem; margin-left: 10rem;" style="width: 18rem;">
+    <img src="\Imagenes\magic.jpg" class="card-img-top" alt="Insert image here">
+    <div class="card-header">
+      <h5 class="card-title">¿Porque leer?</h5>
+      <p class="card-text">Alimenta la inspiración y hace que surjan ideas. Nos permite conectar y ponernos en la piel de otras personas / personajes. Ejercita a nuestro cerebro: despierta vías neuronales, activa la memoria… Nos hace recordar, conocer y aprender</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</div>
+
+
+@stop
