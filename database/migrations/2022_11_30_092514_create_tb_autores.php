c72d6cdb024e9ddbe50a34fd6a54bd28ec7a9424
ElJeloos <120036576@upq.edu.mx>
2022-11-30 03:29:45 -0600

    Migracion BD
    Creacion de la base de datos y las tablas de autores y libros

---
+++ database/migrations/2022_11_30_092514_create_tb_autores.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_autores', function (Blueprint $table) {
+            $table->increments('idAutor');
+            $table->string('Nombre');
+            $table->date('fecha');
+            $table->integer('numPag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_autores');
+    }
+};
